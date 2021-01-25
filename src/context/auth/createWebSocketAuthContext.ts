18568ce4a9d4acb472a7a329522dbe28643dc712
Julian <juliantellezmendez@gmail.com>
2021-01-25 07:47:43 +0000

    chore(auth): create web key client

--- src/context/auth/createWebSocketAuthContext.ts
+++ src/context/auth/createWebSocketAuthContext.ts
@@ -1,32 +1,21 @@
 import { ExpressContext } from 'apollo-server-express/dist/ApolloServer'
-import jsonWebKeySet from 'jwks-rsa'
 
 import getEnv from '../../env'
 
 import createAuthContext, { ContextAuth } from './createAuthContext'
-import createPublicKeyHandler from './createPublicKeyHandler'
+import { webKeyClient } from './webKeyClient'
+
+const {
+  auth0: { audience },
+} = getEnv()
 
 const createWebSocketAuthContext = async (
   context: ExpressContext
 ): Promise<ContextAuth> => {
-  const {
-    auth0: { audience, uri },
-  } = getEnv()
-
   return await createAuthContext({
     audience,
     algorithms: ['RS256'],
-    publicKey: createPublicKeyHandler(jsonWebKeySet.expressJwtSecret, {
-      jwksUri: uri,
-      jwksRequestsPerMinute: 30,
-
-      rateLimit: true,
-      timeout: 30000, // 30s
-
-      cache: true,
-      cacheMaxEntries: 5, // Default value
-      cacheMaxAge: 1800000, // 30m
-    }),
+    publicKey: webKeyClient.getKey,
   })(context)
 }
 
