08638bb122aa656e43841d7b0328e0a0a1ec8327
Julian <juliantellezmendez@gmail.com>
2021-01-27 07:21:40 +0000

    chore: relocate error handler

---
+++ src/middlewares/error/ErrorHandler.ts
@@ -0,0 +1,43 @@
+import { NextObserver, Subject, Subscription } from 'rxjs'
+
+import { logger } from '../../logger'
+
+import { ErrorType } from './ErrorType'
+
+interface AppError {
+  type: ErrorType
+  hint?: string
+  source?: Error | string
+}
+
+class ErrorHandler {
+  private reporter$ = new Subject<AppError>()
+
+  public handleError = (error: AppError): void => {
+    this.reporter$.next({
+      type: error.type,
+      hint: error.hint,
+      source: this.createErrorSource(error.source),
+    })
+  }
+
+  public subscribe = (listener: NextObserver<AppError>): Subscription => {
+    return this.reporter$.subscribe(listener)
+  }
+
+  private createErrorSource = (source: AppError['source']) => {
+    if (!source) return ''
+
+    return typeof source === 'string'
+      ? source
+      : JSON.stringify(source, Object.getOwnPropertyNames(source))
+  }
+}
+
+/**
+ * errorHandler is a singleton
+ */
+const errorHandler = new ErrorHandler()
+errorHandler.subscribe({ next: (e) => logger.error(e) })
+
+export { ErrorHandler, errorHandler, AppError }
